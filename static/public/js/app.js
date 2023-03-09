d93f47f31c981aba8cead6611d3aadb43259b29b
sergejzinaev <zinyaevsi@gmail.com>
2023-03-09 23:09:19 +0500

    interface

--- static/public/js/app.js
+++ static/public/js/app.js
@@ -3,18 +3,29 @@ const inputFile = document.getElementById("file");
 const inputValue = document.getElementById("value");
 
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
 	event.preventDefault();
 
 	const formData = new FormData();
+	const error = null
 
 	formData.append("upload", inputFile.files[0]);
 	formData.append("airac", inputValue.value);
-
-	fetch("http://localhost:3000/api/v1/task", {
+	let result = await fetch("http://localhost:3000/api/v1/task", {
 		method: "post",
 		body: formData,
 	}).catch((error) => ("Something went wrong!", error));
+	let apiResponse = await result.json();
+
+	if (error) {
+		document.getElementById('apiResponse').innerHTML = JSON.stringify(error);
+	} else {
+		if (apiResponse["message"] !== undefined) {
+			document.getElementById('apiResponse').innerHTML = JSON.stringify(apiResponse["message"]);
+		} else {
+			document.getElementById('apiResponse').innerHTML = JSON.stringify(apiResponse["msg"]);
+		}
+	}
 };
 
 form.addEventListener("submit", handleSubmit);
