5a3b66c979844970e8752b1b935cdb149e4faf01
sergejzinaev <zinyaevsi@gmail.com>
2023-03-12 15:17:03 +0500

    interface

--- static/public/js/app.js
+++ static/public/js/app.js
@@ -8,38 +8,35 @@ const handleSubmit = async (event) => {
 	event.preventDefault()
 	const formData = new FormData()
 
+
 	for (let i = 0; i < inputFile.files.length; i++) {
 		formData.append("upload", inputFile.files[i])
 	}
 	formData.append("airac", inputValue.value)
 
-	fetch("api/v1/task", {
+	await fetch("api/v1/task", {
 		method: "post",
 		body: formData
-	}).then((response) => {
-		if (response.ok) {
-			return response
-		}
-		throw new Error("Something went wrong")
 	})
-		.then((apiResponse) => {
-			if (apiResponse.code === 200) {
+		.then(response => response.json())
+		.then(json => {
+			if (json["message"] !== undefined) {
 				inputValue.value = ""
 				inputFile.value = ""
 				notification.style.background = "green"
-				notification.innerHTML = JSON.stringify(apiResponse.json()["message"])
+				notification.innerHTML = JSON.stringify(json["message"])
 				notification.style.display = "block"
 			} else {
 				notification.style.background = "red"
-				notification.innerHTML = JSON.stringify(apiResponse.json()["msg"])
+				notification.innerHTML = JSON.stringify(json["msg"])
 				notification.style.display = "block"
 			}
 		})
-		.catch((error) => {
+		.catch(_ => {
 			notification.style.background = "red"
-			notification.innerHTML = error
+			notification.innerHTML = "Something wrong"
 			notification.style.display = "block"
-		})
+		});
 }
 
 form.addEventListener("submit", handleSubmit)
